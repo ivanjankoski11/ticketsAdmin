bf0817cd1623a531ee35229abc4566b578b5971f
Ivan Jankoski <108348725+ivanjankoski11@users.noreply.github.com>
2023-07-09 20:08:56 +0200

    First commit

---
+++ Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShopAdminApplication.Models
+{
+    public class Ticket
+    {
+        public string MovieName { get; set; }
+
+        public string Genre { get; set; }
+
+        public string TicketPrice { get; set; }
+    }
+}
---
+++ Models/TicketInOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShopAdminApplication.Models
+{
+    public class TicketInOrder
+    {
+        public Guid TicketId { get; set; }
+        public Ticket Ticket { get; set; }
+        public Guid OrderId { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
